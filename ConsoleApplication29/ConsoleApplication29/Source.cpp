2c3ccfff11cdb3eeee86886dec21a0c30b2ce522
Veselinovic <urosveselinovic@outlook.com>
2019-12-08 15:34:02 +0100

    Initial commit

---
+++ ConsoleApplication29/ConsoleApplication29/Source.cpp
@@ -0,0 +1,22 @@
+#define   _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#define MAX 80
+using namespace::std;
+
+int main(){
+
+
+    int	znak, broj;
+    float brojf;
+    char string[MAX + 1];
+	cout << "\n red teksta \n";
+	while ((znak = cin.get())!='\n')
+		cout.put((char)znak);
+	cout.put('\n');
+
+
+
+
+
+
+}
